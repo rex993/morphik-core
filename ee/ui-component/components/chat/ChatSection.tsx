a49ac6857e7bb02945fdd9bb963cd195dba3e8a0
rex993 <6278392+rex993@users.noreply.github.com>
2025-05-17 11:32:27 +1000

    increase chat query max token to 50000

--- ee/ui-component/components/chat/ChatSection.tsx
+++ ee/ui-component/components/chat/ChatSection.tsx
@@ -471,7 +471,7 @@ const ChatSection: React.FC<ChatSectionProps> = ({
                       <Slider
                         id="query-max-tokens"
                         min={1}
-                        max={10240}
+                        max={50000}
                         step={1}
                         value={[safeQueryOptions.max_tokens]}
                         onValueChange={value => safeUpdateOption("max_tokens", value[0])}
